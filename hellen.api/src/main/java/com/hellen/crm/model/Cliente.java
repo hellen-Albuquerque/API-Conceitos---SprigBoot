9641cfbcdb9f0ade751f60c3acce440e973a7e67
hellen-Albuquerque <62775564+hellen-Albuquerque@users.noreply.github.com>
2022-07-05 11:21:41 -0300

    "api estudo"

---
+++ hellen.api/src/main/java/com/hellen/crm/model/Cliente.java
@@ -0,0 +1,69 @@
+package com.hellen.crm.model;
+
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class Cliente {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@Column(length = 50)
+	private String nome;
+	
+	@Column
+	private Double saldo;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(Double saldo) {
+		this.saldo = saldo;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, nome, saldo);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		return Objects.equals(id, other.id) && Objects.equals(nome, other.nome) && Objects.equals(saldo, other.saldo);
+	}
+
+
+}
---
+++ hellen.api/src/test/java/com/hellen/crm/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hellen.crm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
